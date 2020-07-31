dd764796a7fdf2383094901fd1f56b3c018b14a0
roylez <me@roylez.info>
2020-07-31 18:42:16 +1000

    remove lib dir

--- lib/acpi/button_power.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# vim:fdm=marker
-#Author: Roy L Zuo (roylzuo at gmail dot com)
-#Description: 
-
-/usr/bin/aplay /home/roylez/.sounds/fanqiejiayuan-close.wav 
-wait
-/usr/bin/systemctl halt
--- lib/acpi/music.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-script_dir=/home/roylez/bin
-
-export DISPLAY=:0
-
-su roylez -c "$script_dir/mm"
