571d33329d41bbd256d81138f61b67dd5048e2b4
Roy Zuo <me@roylez.info>
2024-07-25 12:23:17 +1000

    kitty menu action

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -4,6 +4,27 @@ vim.cmd([[
   source ~/.vimrc
 ]])
 
+--- {{{ osc52 copy/paste
+vim.o.clipboard = "unnamedplus"
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
+--- }}}
+
 --- {{{ Lazy.nvim
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
