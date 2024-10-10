9e79b40c68867036c2182a210a5cc7f61bfc911e
Roy Zuo <me@roylez.info>
2024-10-11 06:03:39 +1000

    add elixir text object definition for vim

--- vim/.config/nvim/lua/plugins/text-object.lua
+++ vim/.config/nvim/lua/plugins/text-object.lua
@@ -1,5 +1,17 @@
 return {
+
   'kana/vim-textobj-user',
-  'kana/vim-textobj-indent',
+
+  {
+    'kana/vim-textobj-indent',
+    dependencies = { 'kana/vim-textobj-user' }
+  },
+
+  {
+    'andyl/vim-textobj-elixir',
+    ft = 'elixir',
+    dependencies = { 'kana/vim-textobj-user' }
+  },
+
 }
 
