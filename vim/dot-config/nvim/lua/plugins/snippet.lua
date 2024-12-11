fa0dffafa920c33cb385413ec0947496432f58b8
Roy Zuo <me@roylez.info>
2024-12-11 11:06:49 +1000

    use dot-config for vim config dir

---
+++ vim/dot-config/nvim/lua/plugins/snippet.lua
@@ -0,0 +1,16 @@
+return {
+  'dcampos/nvim-snippy',
+  config = function()
+    require('snippy').setup({
+      mappings = {
+        is = {
+          ['<Tab>'] = 'expand_or_advance',
+          ['<S-Tab>'] = 'previous',
+        },
+        nx = {
+          ['<leader>x'] = 'cut_text',
+        },
+      },
+    })
+  end
+}
