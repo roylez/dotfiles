a26d6fe62459ea4ee91486fe48821ea81a7043db
Roy Zuo <me@roylez.info>
2025-01-04 15:31:01 +1000

    add mini.nvim

---
+++ vim/dot-config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,24 @@
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+
+    require('mini.diff').setup()
+
+    require('mini.animate').setup()
+
+    -- commenting
+    require('mini.comment').setup({
+      mappings = {
+        comment = 'gc',
+        comment_line = '<F9>',
+        comment_visual = '<F9>',
+        text_object = 'gc'
+      }
+    })
+
+    -- auto change root directory
+    require('mini.misc').setup()
+    MiniMisc.setup_auto_root({".git", "mix.exs", "Makefile"})
+
+  end
+}
--- vim/dot-config/nvim/lua/plugins/project.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ahmedkhalf/project.nvim",
-  config = function()
-    require("project_nvim").setup({
-      patterns = { ".git", "Makefile", "mix.exs" },
-    })
-  end
-}
