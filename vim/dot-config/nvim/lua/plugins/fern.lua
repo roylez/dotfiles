4fc699298a314d3c828d92d623376b1f45ea38e1
Roy Zuo <me@roylez.info>
2025-01-06 08:12:27 +1000

    keybinding to locate current file in fern

--- vim/dot-config/nvim/lua/plugins/fern.lua
+++ vim/dot-config/nvim/lua/plugins/fern.lua
@@ -17,6 +17,7 @@ return {
     vim.cmd [[
 
     noremap <silent> <F4> :Fern . -drawer -reveal=% -toggle -width=35<CR><C-w>=
+    noremap <silent> z=   :Fern . -drawer -reveal=%<CR>
 
     function! FernInit() abort
       nmap <buffer><expr>
