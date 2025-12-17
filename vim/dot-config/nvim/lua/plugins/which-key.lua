be60a638ca43571ae1e622e02c261db5c6e55010
roylez <me@roylez.info>
2025-12-17 10:27:25 +1000

    remove custom which-key.nvim config function

--- vim/dot-config/nvim/lua/plugins/which-key.lua
+++ vim/dot-config/nvim/lua/plugins/which-key.lua
@@ -1,24 +1,24 @@
 return {
   "folke/which-key.nvim",
-  config = function()
-    require("which-key").setup {
-      preset = "helix",
-      icons = {
-        breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-        separator = "->", -- symbol used between a key and it's label
-        group = "+", -- symbol prepended to a group
-      },
-      layout = {
-        height = { min = 3, max = 25 },
-        spacing = 3,
-        align = 'center'
-      },
-      win = {
-        border = 'single'
-      }
+  lazy = false,
+  opts = {
+    preset = "helix",
+    icons = {
+      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+      separator = "->", -- symbol used between a key and it's label
+      group = "+", -- symbol prepended to a group
+    },
+    layout = {
+      height = { min = 3, max = 25 },
+      spacing = 3,
+      align = 'center'
+    },
+    win = {
+      border = 'single'
     }
-
-    vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y',  { silent = true, desc = 'Copy' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p',  { silent = true, desc = 'Paste' })
-  end
+  },
+  keys = {
+    { '<leader>y', '"+y',  desc = 'Copy',  silent = true, mode = {'n', 'v'} },
+    { '<leader>p', '"+p',  desc = 'Paste', silent = true, mode = {'n', 'v'} },
+  }
 }
