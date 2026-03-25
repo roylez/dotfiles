a56b113ca693264ec781c08eebaad8d158341f69
roylez <me@roylez.info>
2026-03-25 19:37:46 +1000

    some tweaks to kitty scrollback setup

--- vim/dot-config/nvim/init.lua
+++ vim/dot-config/nvim/init.lua
@@ -20,7 +20,7 @@ end
 --- }}}
 
 -- {{{ setting modes
-vim.g.neovim_mode = vim.env.NEOVIM_MODE or "default"
+vim.g.neovim_mode = vim.env.NEOVIM_MODE or vim.g.neovim_mode
 -- }}}
 
 --- {{{ Lazy.nvim
@@ -107,22 +107,62 @@ vim.keymap.set('n', '<Leader>c', toggle_quickfix, { desc = "Toggle Quickfix Wind
 
 --- {{{ pager mode for kitty
 if vim.g.neovim_mode == "pager" then
-  vim.cmd [[
-    colorscheme vim
-    set eventignore=FileType
-    set readonly nolist nomodifiable nospell syntax=
-    set clipboard=unnamed,unnamedplus
-    set laststatus=0
-  ]]
-
+  vim.opt.encoding='utf-8'
+  -- Prevent auto-centering on click
+  vim.opt.scrolloff = 0
+  vim.opt.compatible = false
+  vim.opt.number = false
+  vim.opt.relativenumber = false
+  vim.opt.termguicolors = true
+  vim.opt.showmode = false
+  vim.opt.ruler = false
+  vim.opt.signcolumn=no
+  vim.opt.showtabline=0
+  vim.opt.laststatus = 0
+  vim.o.cmdheight = 0
+  vim.opt.showcmd = false
+  vim.opt.scrollback = 100000
+  vim.opt.clipboard:append('unnamedplus')
+  local term_buf = vim.api.nvim_create_buf(true, false)
+  local term_io = vim.api.nvim_open_term(term_buf, {})
   vim.keymap.set({'n', 'v'}, 'q', [[<cmd>q!<cr>]])
   vim.keymap.set({'n', 'v'}, 'y', [[y<cmd>q!<cr>]])
+  local group = vim.api.nvim_create_augroup('kitty+page', {clear = true})
 
-  local b = vim.api.nvim_create_buf(false, true)
-  local chan = vim.api.nvim_open_term(b, {})
-  vim.api.nvim_chan_send(chan, table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n'))
-  vim.api.nvim_win_set_buf(0, b)
+  vim.api.nvim_create_autocmd('ModeChanged', {
+    group = group,
+    buffer = term_buf,
+    callback = function()
+      local mode = vim.fn.mode()
+      if mode == 't' then
+        vim.cmd.stopinsert()
+      end
+    end,
+  })
 
-  vim.cmd.normal("G")
+  vim.api.nvim_create_autocmd('VimEnter', {
+    group = group,
+    pattern = '*',
+    once = true,
+    callback = function(ev)
+      local current_win = vim.fn.win_getid()
+      -- Instead of sending lines individually, concatenate them.
+      local main_lines = vim.api.nvim_buf_get_lines(ev.buf, 0, -2, false)
+      for i, v in ipairs(main_lines) do
+        main_lines[i] = v:gsub("%s+$", "")
+      end
+      local content = table.concat(main_lines, '\r\n')
+      vim.api.nvim_chan_send(term_io, content .. '\r\n')
+
+      -- Process the last line separately (without trailing \r\n)
+      local last_line = vim.api.nvim_buf_get_lines(ev.buf, -2, -1, false)[1]
+      if last_line then
+        vim.api.nvim_chan_send(term_io, last_line)
+      end
+      vim.api.nvim_win_set_buf(current_win, term_buf)
+      vim.api.nvim_buf_delete(ev.buf, { force = true } )
+      vim.cmd.normal("G")
+    end
+  })
 end
 --- }}}
